e025db373b581f169bcc048e34c5eed2b735f61d
Mehmet <mehmetkusken@gmail.com>
2021-08-20 15:35:53 -0600

    commit tree aa5754c655d4d2f09f047f9398a0b8fc18ba2ce6

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your functions here
+function map(array, callback) {
+    let newArray = []
+    for(let i=0; i<array.length; i++){
+        newArray.push(callback(array[i]))
+    }
+    return newArray
+}
+
+function reduce(sourceArray, cb, startingPoint){
+    let runningTotal
+    let i
+    if(startingPoint){
+        runningTotal = startingPoint
+        i = 0
+    } else {
+        runningTotal = sourceArray[0]
+        i = 1
+    }
+
+    for( i; i < sourceArray.length; i++){
+        runningTotal = cb(sourceArray[i], runningTotal)
+    }
+    return runningTotal
+} 
